9ef8cbd09c892a04a2693916c7806fbdfa492f6b
gcirera <gcirera@cbacc964-ccda-11dd-86c2-0f8696b7b6f9>
2009-02-03 14:36:03 +0000

    game state added

---
+++ Karts/Code/GameState.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Karts.Code
+{
+    // ------------------------------------------------
+    // Types
+    // ------------------------------------------------
+    enum EGameStateType
+    {
+        EGM_INVALID     = 0,
+        EGM_MAIN_MENU   = 1,
+        EGM_PAUSE_MENU  = 2,
+        EGM_GAME        = 3
+    }
+
+    class GameState
+    {
+        // ------------------------------------------------
+        // Class members
+        // ------------------------------------------------
+        private bool            m_bActive;
+        private EGameStateType  m_eType;
+
+
+        // ------------------------------------------------
+        // Class methods
+        // ------------------------------------------------
+        public GameState() 
+        {
+            m_bActive = false;
+            m_eType = EGameStateType.EGM_INVALID;
+        }
+
+        ~GameState() { }
+
+        public bool IsActive()  { return m_bActive; }
+        public EGameStateType GetType()   { return m_eType; }
+
+        public void Update(GameTime GameTime)
+        {
+        }
+
+        public void Draw(GameTime GameTime)
+        {
+        }
+    }
+}
